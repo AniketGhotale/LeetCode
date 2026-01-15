648375fa4a26b9e1c31fb42b8275eca273875830
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-16 00:11:26 +0530

    Time: 2 ms (98.86%), Space: 44.2 MB (87.76%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,59 @@
+// class Solution {
+//     public boolean isPalindrome(String s) {
+//         s = s.toLowerCase();
+//         int first=0;
+//         int last = s.length()-1;
+//         if(s == ".,"){
+//             return false;
+//         }
+//         while(first <last){
+//             while(s.charAt(first) < 'a' || s.charAt(first) > 'z'){
+//                 first++;
+//             }
+//             while(s.charAt(last) < 'a' || s.charAt(last) > 'z'){
+//                 last--;
+//             }
+//             if(s.charAt(first) != s.charAt(last)){
+//                 return false;
+//             }else{
+//                 first++;
+//                 last--;
+//             }
+
+//         }
+//         return true;
+//     }
+// }
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+
+            // skip non-alphanumeric from left
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
+            }
+
+            // skip non-alphanumeric from right
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+
+            // compare after converting to lowercase
+            if (Character.toLowerCase(s.charAt(left)) !=
+                Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+}
