8e622637a9769f00db5f2ac116aaaf44307925c0
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-22 12:03:55 +0530

    Time: 0 ms (100%), Space: 44.4 MB (24.86%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -5,23 +5,11 @@ class Solution {
         if(nums.length == 1){
             return 0;
         }
-        int mid = 0;
         while(low < high){
-            mid = low + (high-low) /2;
-            // if(mid == 0){
-            //     if(nums[mid] > nums[mid+1]){
-            //         return mid;
-            //     }   
-            // }
-            // if(mid == nums.length-1){
-            //     if(nums[mid] > nums[mid-1]){
-            //         return mid;
-            //     }   
-            // }
+            int mid = low + (high-low) /2;
             if(nums[mid] > nums[mid+1]){
-                
                 high = mid;
-            }else if(nums[mid] < nums[mid+1]){
+            }else{
                 low = mid+1;
             }
         } 
