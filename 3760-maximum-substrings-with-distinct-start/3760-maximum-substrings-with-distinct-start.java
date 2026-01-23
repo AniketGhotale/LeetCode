64431dfaa1320fce4a64b13e23bd94f1c7675777
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-23 11:39:33 +0530

    Time: 54 ms (5.58%), Space: 48.2 MB (15.88%) - LeetHub

--- 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -6,14 +6,16 @@ class Solution {
             map.put(s.charAt(i),(map.getOrDefault(s.charAt(i),0)+1));
         }
 
-        for(int i=0; i<s.length(); i++){
-            if(map.get(s.charAt(i))==1 ){
-                count++;
-            }else{
-                map.put(s.charAt(i),(map.get(s.charAt(i))-1));
-            }
-        }
+        return map.size();
 
-        return count;
+        // for(int i=0; i<s.length(); i++){
+        //     if(map.get(s.charAt(i))==1 ){
+        //         count++;
+        //     }else{
+        //         map.put(s.charAt(i),(map.get(s.charAt(i))-1));
+        //     }
+        // }
+
+        // return count;
     }
 }
