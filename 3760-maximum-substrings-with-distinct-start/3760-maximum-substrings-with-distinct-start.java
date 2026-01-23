caf3f8c918eb05e345f7ba0d343984494c097d1e
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-23 11:36:07 +0530

    Time: 122 ms (5.58%), Space: 47.4 MB (98.27%) - LeetHub

---
+++ 3760-maximum-substrings-with-distinct-start/3760-maximum-substrings-with-distinct-start.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxDistinct(String s) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        int count=0;
+        for(int i=0; i<s.length(); i++){
+            map.put(s.charAt(i),(map.getOrDefault(s.charAt(i),0)+1));
+        }
+
+        for(int i=0; i<s.length(); i++){
+            if(map.get(s.charAt(i))==1 ){
+                count++;
+            }else{
+                map.put(s.charAt(i),(map.get(s.charAt(i))-1));
+            }
+        }
+
+        return count;
+    }
+}
