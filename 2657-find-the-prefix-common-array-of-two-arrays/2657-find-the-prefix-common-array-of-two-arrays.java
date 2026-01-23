1acb9c165f27d325a7ea943ebfceb2e468fc4158
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-23 15:38:26 +0530

    Time: 5 ms (45.08%), Space: 47.2 MB (91.37%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.java
@@ -4,22 +4,22 @@ class Solution {
         HashSet<Integer> aset = new HashSet<>();
         HashSet<Integer> bset = new HashSet<>();
         int res[] = new int[A.length];
-        
+        int count = 0;
         for(int i=0; i<A.length; i++){
             aset.add(A[i]);
-            // if(bset.contains(A[i])){
-            //     count++;
-            // }
-            bset.add(B[i]);
-            // if(aset.contains(B[i])){
-            //     count++;
-            // }
-            int count = 0;
-            for (int val : aset) {
-                if (bset.contains(val)) {
-                    count++;
-                }
+            if(bset.contains(A[i])){
+                count++;
             }
+            bset.add(B[i]);
+            if(aset.contains(B[i])){
+                count++;
+            }
+            // int count = 0;
+            // for (int val : aset) {
+            //     if (bset.contains(val)) {
+            //         count++;
+            //     }
+            // }
             res[i] = count;
         }   
 
