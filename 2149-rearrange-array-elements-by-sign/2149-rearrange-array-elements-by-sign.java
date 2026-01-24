0fad8747796b683bced9624b4a708618af85d3fa
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-24 13:47:37 +0530

    Time: 4 ms (44.99%), Space: 175.7 MB (34.9%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -2,21 +2,29 @@ class Solution {
     public int[] rearrangeArray(int[] nums) {
         int res[] = new int[nums.length];
         int idxpos = 0;
-        for(int i=0; i<nums.length; i+=2){
-            while(nums[idxpos] < 0 && idxpos < nums.length){
-                idxpos++;
+        int idxneg = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > 0){
+                res[idxpos] = nums[i];
+                idxpos+=2;
+            }else{
+                res[idxneg] = nums[i];
+                idxneg+=2;
             }
-            res[i] = nums[idxpos];
-            idxpos++;
+            // while(nums[idxpos] < 0 && idxpos < nums.length){
+            //     idxpos++;
+            // }
+            // res[i] = nums[idxpos];
+            // idxpos++;
         }
-        int idxneg = 0;
-        for(int i=1; i<nums.length; i+=2){
-            while(nums[idxneg] > 0 && idxneg < nums.length){
-                idxneg++;
-            }
-            res[i] = nums[idxneg];
-            idxneg++;
-        }
+        // int idxneg = 0;
+        // for(int i=1; i<nums.length; i+=2){
+        //     while(nums[idxneg] > 0 && idxneg < nums.length){
+        //         idxneg++;
+        //     }
+        //     res[i] = nums[idxneg];
+        //     idxneg++;
+        // }
         return res;
     }
 }
