813c6311defc67f94e53a6f671ac5858cecb34f9
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-25 13:37:26 +0530

    Time: 0 ms (100%), Space: 45 MB (69.69%) - LeetHub

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -1,17 +1,8 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        HashSet<Integer> set = new HashSet<>();
-        int num1s = Integer.MAX_VALUE;
-        int num2s = Integer.MAX_VALUE;
-        for(int i=0; i<nums1.length; i++){
-            // set.add(nums2[i]);
-            // for(int j=0; j<nums1.length; j++){
-            //     if(set.contains(nums2[i] - nums1[j])){
-            //         return nums2[i] - nums1[j];
-            //     }else{
-            //         set.add(nums2[i] - nums1[j]);
-            //     }
-            // }
+        int num1s = nums1[0];//Integer.MAX_VALUE;
+        int num2s = nums2[0]; //Integer.MAX_VALUE;
+        for(int i=1; i<nums1.length; i++){
             if(nums1[i] < num1s){
                 num1s = nums1[i];
             }
@@ -19,13 +10,6 @@ class Solution {
                 num2s = nums2[i];
             }
         }
-        // for(int i=0; i<1000; i++){
-        //     if(set.contains(nums1[0] + i)){
-        //         return i;
-        //     }else if(set.contains(nums1[0] - i)){
-        //         return -i;
-        //     }
-        // }
         return num2s - num1s;
     }
 }
