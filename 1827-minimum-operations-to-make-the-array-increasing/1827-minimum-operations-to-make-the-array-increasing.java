f9fa0921fb595c8c61f1450987ae187b3c58cb10
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-25 23:43:28 +0530

    Time: 5 ms (24.43%), Space: 47.3 MB (15.14%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -5,15 +5,15 @@ class Solution {
         if(n == 1){
             return 0;
         }
-        for(int i=0; i<nums.length-1; i++){
-            if(nums[i] >= nums[i+1]){
-                count += (nums[i] - nums[i+1]) +1;
-                nums[i+1] += (nums[i] - nums[i+1]) +1;
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] <= nums[i-1]){
+                count += (nums[i-1] - nums[i]) +1;
+                nums[i] += (nums[i-1] - nums[i]) +1;
             }
         }
-        if(nums[n-2] >= nums[n-1]){
-            count += (nums[n-2] - nums[n-1]) +1;
-        }
+        // if(nums[n-2] >= nums[n-1]){
+        //     count += (nums[n-2] - nums[n-1]) +1;
+        // }
         return count;
     }
 }
