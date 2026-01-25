c31856f0ce10fb709463654ac916c994840fa797
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-25 23:44:37 +0530

    Time: 5 ms (24.43%), Space: 47.4 MB (5.22%) - LeetHub

--- 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
+++ 1827-minimum-operations-to-make-the-array-increasing/1827-minimum-operations-to-make-the-array-increasing.java
@@ -1,10 +1,9 @@
 class Solution {
     public int minOperations(int[] nums) {
         int count = 0;
-        int n = nums.length;
-        if(n == 1){
-            return 0;
-        }
+        // if(n == nums.length){
+        //     return 0;
+        // }
         for(int i=1; i<nums.length; i++){
             if(nums[i] <= nums[i-1]){
                 count += (nums[i-1] - nums[i]) +1;
