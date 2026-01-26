4ac08abce40ebb6b73ec2efcce64fcb60a0e2a4e
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-27 00:57:40 +0530

    Time: 1 ms (51.54%), Space: 43.8 MB (12.82%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -1,20 +1,35 @@
 class Solution {
     public int countPartitions(int[] nums) {
        int count = 0;
+       int sum = 0;
+       for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+       }
+       if(sum % 2 != 0){
+        return 0;
+       }
+       int left = 0;
        for(int i=0; i<nums.length-1; i++){
-            int leftsum = 0;
-            int rightsum = 0;
-            for(int j=0; j<=i; j++){
-                leftsum += nums[j];
-            }
-            for(int k=i+1; k<nums.length; k++){
-                rightsum += nums[k];
-            }
-            int res = leftsum-rightsum;
-            if(res % 2 == 0){
-                count++;
-            }
-       } 
+        left += nums[i];
+        int right = sum - left;
+        if( (left - right) % 2 ==0){
+            count++;
+        }
+       }
+    //    for(int i=0; i<nums.length-1; i++){
+    //         int leftsum = 0;
+    //         int rightsum = 0;
+    //         for(int j=0; j<=i; j++){
+    //             leftsum += nums[j];
+    //         }
+    //         for(int k=i+1; k<nums.length; k++){
+    //             rightsum += nums[k];
+    //         }
+    //         int res = leftsum-rightsum;
+    //         if(res % 2 == 0){
+    //             count++;
+    //         }
+    //    } 
        return count;
     }
 }
