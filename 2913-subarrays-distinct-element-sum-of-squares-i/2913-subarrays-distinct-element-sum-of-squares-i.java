8c1925be67b93f81278cb4c0a829de8733a20fbf
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-26 17:29:58 +0530

    Time: 7 ms (89.47%), Space: 46.6 MB (80%) - LeetHub

--- 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.java
+++ 2913-subarrays-distinct-element-sum-of-squares-i/2913-subarrays-distinct-element-sum-of-squares-i.java
@@ -1,12 +1,14 @@
 class Solution {
     public int sumCounts(List<Integer> nums) {
         int sum = 0;
+        HashSet<Integer> set = new HashSet<>();
         for(int i=0; i<nums.size(); i++){
-            HashSet<Integer> set = new HashSet<>();
+            
             for(int j=i; j<nums.size(); j++){
                 set.add(nums.get(j));
                 sum += set.size() * set.size();
             }
+            set.clear();
         }
         return sum;
     }
