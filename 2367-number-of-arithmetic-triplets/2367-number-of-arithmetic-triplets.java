59e5423b03ed0892f7bd85bda3a3ce2ad77c1d69
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-27 00:33:49 +0530

    Time: 2 ms (87.61%), Space: 43.9 MB (7.8%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.java
@@ -1,15 +1,26 @@
 class Solution {
     public int arithmeticTriplets(int[] nums, int diff) {
         int count = 0;
+        HashSet<Integer> set = new HashSet<>();
         for(int i=0; i<nums.length; i++){
-            for(int j=i+1; j<nums.length; j++){
-                for(int k=j+1; k<nums.length; k++){
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
-                        count++;
-                    }
-                }
+            set.add(nums[i]);
+        }
+        for(int i=1; i<nums.length; i++){
+            int left = nums[i]-diff;
+            int right = nums[i]+diff;
+            if(set.contains(left) && set.contains(right)){
+                count++;
             }
         }
+        // for(int i=0; i<nums.length; i++){
+        //     for(int j=i+1; j<nums.length; j++){
+        //         for(int k=j+1; k<nums.length; k++){
+        //             if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff){
+        //                 count++;
+        //             }
+        //         }
+        //     }
+        //}
         return count;
     }
 }
