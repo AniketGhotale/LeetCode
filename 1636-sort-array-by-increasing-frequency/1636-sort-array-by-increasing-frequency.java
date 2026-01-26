8bb1956d79c15cc77f7d55fff62e23ecd1c86e28
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-26 16:50:56 +0530

    Time: 9 ms (39.7%), Space: 45.8 MB (32.3%) - LeetHub

---
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int[] frequencySort(int[] nums) {
+
+        // 1. Count frequency
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        // 2. Convert nums to Integer array for sorting
+        Integer[] arr = new Integer[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i] = nums[i];
+        }
+
+        // 3. Sort based on frequency, then value (descending)
+        Arrays.sort(arr, (a, b) -> {
+            if (!map.get(a).equals(map.get(b))) {
+                return map.get(a) - map.get(b);   // increasing frequency
+            }
+            return b - a;                          // decreasing value
+        });
+
+        // 4. Convert back to int[]
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = arr[i];
+        }
+
+        return nums;
+    }
+}
+// class Solution {
+//     public int[] frequencySort(int[] nums) {
+//         HashMap<Map.Entry<Integer,Integer>> map = new HashMap<>(map.entrySet());
+//         for(int i=0; i<nums.length; i++){
+//             map.put(nums[i],(map.getOrDefault(nums[i],0)+1));
+//         }
+//         map.sort((a, b) -> {
+//         int cmp = a.getValue().compareTo(b.getValue());
+//         if (cmp != 0) return cmp;
+//         return a.getKey().compareTo(b.getKey());
+//         });
+//         int res[] = new int[nums.length];
+//         int idx = 0;
+//         for(int ele : map){
+//             if(map.get(ele) <= 1){
+//                 res[idx] = ele;
+//                 idx++;
+//             }else{
+//                 while(map.get(ele) != 0){
+//                     res[idx] = ele;
+//                     idx++;
+//                     map.put(ele,map.get(ele)-1);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// }
