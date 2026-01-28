6b49266a383078bd2ec55a097c30c7cff875dea3
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-28 13:37:24 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.97%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.java
@@ -1,21 +1,22 @@
 class Solution {
-    public boolean palindrome(String str){
-        StringBuilder sb=new StringBuilder(str);
-        return str.equals(sb.reverse().toString());
-    }
+    // public boolean palindrome(String str){
+    //     StringBuilder sb=new StringBuilder(str);
+    //     return str.equals(sb.reverse().toString());
+    // }
     public boolean isStrictlyPalindromic(int n) {
-        for(int i=2;i<=n-2;i++){
-            StringBuilder sb=new StringBuilder();
-            int temp=n;
-            while(temp!=0){
-                sb.append(temp%i);
-                temp/=i;
-            }
-            String str=sb.toString();
-            if(!palindrome(str)){
-                return false;
-            }
-        }
-        return true;
+        return false;
+        // for(int i=2;i<=n-2;i++){
+        //     StringBuilder sb=new StringBuilder();
+        //     int temp=n;
+        //     while(temp!=0){
+        //         sb.append(temp%i);
+        //         temp/=i;
+        //     }
+        //     String str=sb.toString();
+        //     if(!palindrome(str)){
+        //         return false;
+        //     }
+        // }
+        // return true;
     }
 }
