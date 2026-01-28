246049a422a5d30c9ae431c6e185889f499c223d
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-28 23:57:39 +0530

    Time: 1 ms (80.19%), Space: 42.8 MB (31.48%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -1,5 +1,24 @@
 class Solution {
     public int gcdOfOddEvenSums(int n) {
-        return n;
+        int a=n*n;
+        int b=n*n + n;
+        int ans=gcd(a,b);
+        return ans;
+    }
+    public int gcd(int a,int b){
+        if(a==0){
+            return b;
+        }
+        if(b==0){
+            return a;
+        }
+        while(a!=b){
+            if(a>b){
+                a=a-b;
+            }else{
+                b=b-a;
+            }
+        }
+        return a;
     }
 }
