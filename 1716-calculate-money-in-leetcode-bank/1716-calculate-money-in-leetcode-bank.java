5fba502952f525a7955fb578e864422bcd5e94b1
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-29 00:34:15 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.69%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -1,37 +1,15 @@
-// class Solution {
-//     public int totalMoney(int n) {
-//         int i=0;
-//         int ans = 0;
-//         while( n >= 7){
-//             ans += 28 + (i * 7);
-//             i++;
-//             n-=7;
-//         }
-//         for(int j=1; j<=n; j++){
-//             ans+=j;
-//         }
-//         return ans;
-//     }
-// }
-
-
 class Solution {
     public int totalMoney(int n) {
         int i = 0;
         int ans = 0;
-
-        // full weeks
         while (n >= 7) {
             ans += 28 + (i * 7);
             i++;
             n -= 7;
         }
-
-        // remaining days
         for (int j = 1; j <= n; j++) {
             ans += j + i;
         }
-
         return ans;
     }
 }
