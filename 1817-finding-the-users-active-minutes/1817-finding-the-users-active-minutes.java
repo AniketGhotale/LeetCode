6036f65766e5c69d51f09aea219fba9f899c7d2f
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-29 14:48:16 +0530

    Time: 19 ms (50.26%), Space: 88.8 MB (26.67%) - LeetHub

---
+++ 1817-finding-the-users-active-minutes/1817-finding-the-users-active-minutes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findingUsersActiveMinutes(int[][] logs, int k) {
+        HashSet<Integer> set = new HashSet<>();
+        HashMap<Integer,HashSet<Integer>> map = new HashMap<>();
+        int res[] = new int[k];
+        for(int i=0; i<logs.length; i++){
+            int user = logs[i][0];
+            int minute = logs[i][1];
+            map.putIfAbsent(user, new HashSet<>());
+            map.get(user).add(minute);
+        }
+        for(int key : map.keySet()){
+            res[map.get(key).size() - 1]++;
+        }
+        return res;
+    }
+}
