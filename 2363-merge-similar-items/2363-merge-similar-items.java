5f23b091ba5dd245287a81a09ab37908694d8d2b
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-30 15:44:08 +0530

    Time: 13 ms (15.62%), Space: 47.6 MB (9.32%) - LeetHub

---
+++ 2363-merge-similar-items/2363-merge-similar-items.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<items1.length; i++){
+            map.put(items1[i][0],(map.getOrDefault(items1[i][0],0)+items1[i][1]));
+        }
+        for(int i=0; i<items2.length; i++){
+            map.put(items2[i][0],(map.getOrDefault(items2[i][0],0)+items2[i][1]));
+        }
+        Map<Integer, Integer> sortedMap = new TreeMap<>(map);
+        for(int key : sortedMap.keySet()){
+            List<Integer> l = new ArrayList<>();
+            l.add(key);
+            l.add(map.get(key));
+            list.add(l);
+        }
+        return list;
+    }   
+}
