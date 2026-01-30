ee428e757f03bac003cfb8aaf827b921112c0445
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-30 12:40:13 +0530

    Time: 2 ms (43.91%), Space: 43.3 MB (72.58%) - LeetHub

--- 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
+++ 1252-cells-with-odd-values-in-a-matrix/1252-cells-with-odd-values-in-a-matrix.java
@@ -4,32 +4,21 @@ class Solution {
         for(int i=0; i<indices.length; i++){
             int currrow = indices[i][0];
             int currcol = indices[i][1];
-            //res[currrow][currcol] += 2;
             for(int j=0; j<n; j++){
                 res[currrow][j] += 1;
             }
             for(int j=0; j<m; j++){
                 res[j][currcol] += 1;
             }
-            // res[currrow-1][currcol] += 1;
-            // res[currrow+1][currcol] += 1;
-            // res[currrow][currcol-1] += 1;
-            // res[currrow][currcol+1] += 1;
-
-            // for(int j=0; j<indices[i].length; j++){
-            //     col = j;
-            //     int currrow = indices[i][0];
-            //     res[indices[row]][indices[col]] += 2;
-
-            }
-            int odd = 0; 
-            for(int i=0; i<res.length;i++){
-                for(int j=0; j<res[i].length; j++){
-                    if(res[i][j] %2 == 1){
-                        odd++;
-                    }
+        }
+        int odd = 0; 
+        for(int i=0; i<res.length;i++){
+            for(int j=0; j<res[i].length; j++){
+                if(res[i][j] %2 == 1){
+                    odd++;
                 }
             }
-            return odd;
+        }
+        return odd;
     }
 }
