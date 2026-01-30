4288f8f3a0308cda0ef55a5d1c9521e3b50294a9
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-30 11:31:28 +0530

    Time: 1 ms (37.13%), Space: 44.1 MB (85.09%) - LeetHub

---
+++ 2079-watering-plants/2079-watering-plants.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int wateringPlants(int[] plants, int capacity) {
+        int count = 0;
+        int org = capacity;
+        for(int i=0; i<plants.length; i++){
+            if(capacity >= plants[i]){
+                capacity -= plants[i];
+                count++;
+            }else{
+                count+=i;
+                capacity = org;
+                count+=(i+1);
+                capacity -= plants[i];
+            }
+        }
+        return count;
+    }
+}
