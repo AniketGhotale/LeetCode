b420f6ddbadced360586ec04c06ba51fa6651ba3
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-30 15:51:07 +0530

    Time: 4 ms (86.15%), Space: 47.2 MB (85.89%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.java
+++ 2363-merge-similar-items/2363-merge-similar-items.java
@@ -1,44 +1,31 @@
-// class Solution {
-//     public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
-//         HashMap<Integer,Integer> map = new HashMap<>();
-//         List<List<Integer>> list = new ArrayList<>();
-//         for(int i=0; i<items1.length; i++){
-//             map.put(items1[i][0],(map.getOrDefault(items1[i][0],0)+items1[i][1]));
-//         }
-//         for(int i=0; i<items2.length; i++){
-//             map.put(items2[i][0],(map.getOrDefault(items2[i][0],0)+items2[i][1]));
-//         }
-//         Map<Integer, Integer> sortedMap = new TreeMap<>(map);
-//         for(int key : sortedMap.keySet()){
-//             List<Integer> l = new ArrayList<>();
-//             l.add(key);
-//             l.add(map.get(key));
-//             list.add(l);
-//         }
-//         return list;
-//     }   
-// }
-
-
 class Solution {
-        public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
-            int [] sums = new int[1001];
-            for (int i = 0; i < items1.length; i++) {
-                sums[items1[i][0]] += items1[i][1];
-            }
-
-            for (int i = 0; i < items2.length; i++) {
-                sums[items2[i][0]] += items2[i][1];
-            }
-            List<List<Integer>> r = new ArrayList<>();
-            for (int i = 0; i < sums.length; i++) {
-                if (sums[i] > 0){
-                    var c = new ArrayList<Integer>();
-                    c.add(i);
-                    c.add(sums[i]);
-                    r.add(c);
-                }
-            }
-            return r;
+    public List<List<Integer>> mergeSimilarItems(int[][] items1, int[][] items2) {
+        //HashMap<Integer,Integer> map = new HashMap<>();
+        int sum[] = new int[1001];
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<items1.length; i++){
+            sum[items1[i][0]] += items1[i][1];
+            //map.put(items1[i][0],(map.getOrDefault(items1[i][0],0)+items1[i][1]));
         }
-    }
+        for(int i=0; i<items2.length; i++){
+            sum[items2[i][0]] += items2[i][1];
+            //map.put(items2[i][0],(map.getOrDefault(items2[i][0],0)+items2[i][1]));
+        }
+        //Map<Integer, Integer> sortedMap = new TreeMap<>(map);
+        // for(int key : sortedMap.keySet()){
+        //     List<Integer> l = new ArrayList<>();
+        //     l.add(key);
+        //     l.add(map.get(key));
+        //     list.add(l);
+        // }
+        for(int i=0; i<sum.length; i++){
+            if(sum[i] > 0){
+                List<Integer> l = new ArrayList<>();
+                l.add(i);
+                l.add(sum[i]);
+                list.add(l);
+            }
+        }
+        return list;
+    }   
+}
