8abe06841ccc2fdbf5c4e71a06e0b4a588ae3f27
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-01-30 13:37:02 +0530

    Time: 0 ms (100%), Space: 44.8 MB (83.51%) - LeetHub

--- 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.java
@@ -2,11 +2,12 @@ class Solution {
     public int countTestedDevices(int[] batteryPercentages) {
         int count = 0;
         for(int i=0; i<batteryPercentages.length; i++){
-            if(batteryPercentages[i] > 0){
-                count++;
-                for(int j=i+1; j<batteryPercentages.length; j++){
-                    batteryPercentages[j] = Math.max(0,batteryPercentages[j]-1);
-                }
+            if(batteryPercentages[i] - count > 0){
+                count+=1;
+                // count++;
+                // for(int j=i+1; j<batteryPercentages.length; j++){
+                //     batteryPercentages[j] = Math.max(0,batteryPercentages[j]-1);
+                // }
             }
         }
         return count;
