b366564b78ad434760f6f00849a72c59e317634e
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-07 00:20:12 +0530

    Time: 1 ms (100%), Space: 46.4 MB (81.89%) - LeetHub

--- 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
+++ 2586-count-the-number-of-vowel-strings-in-range/2586-count-the-number-of-vowel-strings-in-range.java
@@ -1,9 +1,12 @@
 class Solution {
     public int vowelStrings(String[] words, int left, int right) {
         int count = 0;
+        String vowel = "aeiou";
         while(left <= right){
             int n = words[left].length()-1;
-            if( (words[left].charAt(0) == 'a' || words[left].charAt(0) == 'e' || words[left].charAt(0) == 'i' || words[left].charAt(0) == 'o' || words[left].charAt(0) == 'u')  && (words[left].charAt(n) == 'a' || words[left].charAt(n) == 'e' || words[left].charAt(n) == 'i' || words[left].charAt(n) == 'o' || words[left].charAt(n) == 'u' ) ){
+            char first = words[left].charAt(0);
+            char last = words[left].charAt(n);
+            if(vowel.indexOf(first) != -1 && vowel.indexOf(last) != -1){
                 count++;
             }
             left++;
