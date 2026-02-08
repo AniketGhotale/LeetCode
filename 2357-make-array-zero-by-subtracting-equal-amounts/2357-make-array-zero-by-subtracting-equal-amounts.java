a5f158ec3c5e2a66200f9082c958f3704726c56f
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-09 00:22:14 +0530

    Time: 5 ms (13.14%), Space: 43.5 MB (10.57%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.java
@@ -1,21 +1,22 @@
 class Solution {
     public int minimumOperations(int[] nums) {
-        Set<Integer> set=new HashSet<>();
+        //Set<Integer> set=new HashSet<>();
         Arrays.sort(nums);
         int count = 0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i] == 0){
-                continue;
-            }
-            set.add(nums[i]);
-            // if(nums[i]!=0){
-            //     int curr = nums[i];
-            //     count++;
-            //     for(int j=i; j<nums.length;j++){
-            //         nums[j] -= curr; 
-            //     }
+            // if(nums[i] == 0){
+            //     continue;
             // }
-        }   
-        return set.size();
+            // set.add(nums[i]);
+            if(nums[i]!=0){
+                int curr = nums[i];
+                count++;
+                for(int j=i; j<nums.length;j++){
+                    nums[j] -= curr; 
+                }
+            }
+        } 
+        return count;  
+        //return set.size();
     }
 }
