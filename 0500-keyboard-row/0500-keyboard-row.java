1589ef8e618b78065249547e6740f23fe94ffecb
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-09 01:06:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (13.26%) - LeetHub

---
+++ 0500-keyboard-row/0500-keyboard-row.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String[] findWords(String[] words) {
+        String s1 = "qwertyuiop";
+        String s2 = "asdfghjkl";
+        String s3 = "zxcvbnm";
+        int row = -1;
+        List<String> list = new ArrayList<>();
+        for(String s : words){
+            String org = s;
+            s = s.toLowerCase();
+            if(s1.indexOf(s.charAt(0)) != -1){
+                row = 1;
+                for(int i=0; i<s.length(); i++){
+                    if(s1.indexOf(s.charAt(i)) == -1){
+                        break;
+                    }
+                    if(i==s.length()-1){
+                        list.add(org);
+                    }
+                }
+            }else if(s2.indexOf(s.charAt(0)) != -1){
+                row = 2;
+                for(int i=0; i<s.length(); i++){
+                    if(s2.indexOf(s.charAt(i)) == -1){
+                        break;
+                    }
+                    if(i==s.length()-1){
+                        list.add(org);
+                    }
+                }
+            }else{
+                row = 3;
+                for(int i=0; i<s.length(); i++){
+                    if(s3.indexOf(s.charAt(i)) == -1){
+                        break;
+                    }
+                    if(i==s.length()-1){
+                        list.add(org);
+                    }
+                }
+            }
+        }
+        String res[] = new String[list.size()];
+        for(int i=0; i<list.size(); i++){
+            res[i] = list.get(i);
+        }
+        return res;
+    }
+}
