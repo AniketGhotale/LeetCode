063c423b46133e7316703c857dd4db1907f80f68
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-10 00:33:31 +0530

    Time: 5 ms (81.72%), Space: 43 MB (86.56%) - LeetHub

--- 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
+++ 2475-number-of-unequal-triplets-in-array/2475-number-of-unequal-triplets-in-array.java
@@ -3,8 +3,11 @@ class Solution {
         int count = 0;
         for(int i=0; i<nums.length; i++){
             for(int j=i+1; j<nums.length; j++){
+                if(nums[i] == nums[j]){
+                    continue;
+                }
                 for(int k=j+1; k<nums.length; k++){
-                    if(nums[i] != nums[j] && nums[i] != nums[k] && nums[j] != nums[k]){
+                    if(nums[i] != nums[k] && nums[j] != nums[k]){
                         count++;
                     }
                 }
