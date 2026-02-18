ed4a8fcfb75adec48af93799a8a0102f940ae915
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-19 00:54:56 +0530

    Time: 1 ms (99.26%), Space: 43.8 MB (76.72%) - LeetHub

---
+++ 1598-crawler-log-folder/1598-crawler-log-folder.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(String[] logs) {
+        int pos = 0;
+        for(int i=0; i<logs.length; i++){
+            if(logs[i].charAt(0) == '.' && logs[i].charAt(1) == '.'){
+                pos = (pos==0)?0:--pos;
+            }else if(logs[i].charAt(0) == '.' && logs[i].charAt(1) == '/'){
+                pos = pos;
+            }else{
+                pos++;
+            }
+        }
+        return pos;
+    }
+}
