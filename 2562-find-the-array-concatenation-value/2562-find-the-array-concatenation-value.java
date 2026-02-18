c9d6b1b781dd82bd10e52c41f4880c0665cc3f44
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-19 00:17:42 +0530

    Time: 4 ms (73.28%), Space: 46.4 MB (25.71%) - LeetHub

--- 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
+++ 2562-find-the-array-concatenation-value/2562-find-the-array-concatenation-value.java
@@ -4,9 +4,10 @@ class Solution {
         int f = 0;
         int l = nums.length-1;
         while(f < l){
-            // String s1 = Integer.toString(nums[f]);
-            // String s2 = Integer.toString(nums[l]);
-            String s3 = nums[f] + "" + nums[l];
+            String s1 = Integer.toString(nums[f]);
+            String s2 = Integer.toString(nums[l]);
+            String s3 = s1+s2;
+            // String s3 = nums[f] + "" + nums[l];
             ans+=Integer.parseInt(s3);
             f++;
             l--;
