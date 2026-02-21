252bb1b6dea3d3f3682de6c40231bba684ae5cfb
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-22 01:38:55 +0530

    Time: 6 ms (61.83%), Space: 47.9 MB (17.84%) - LeetHub

--- 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.java
+++ 2639-find-the-width-of-columns-of-a-grid/2639-find-the-width-of-columns-of-a-grid.java
@@ -3,11 +3,8 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int res[] = new int[n];
-        int k = 0;
         for(int i=0; i<m; i++){
-            int row = i;
             for(int j=0; j<n; j++){
-                int col = j;
                 int len = Integer.toString(grid[i][j]).length();
                 if(res[j] < len){
                     res[j] = len;
