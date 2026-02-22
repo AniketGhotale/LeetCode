407487c4a6e575e9fbdf810111ecebe2217e6d57
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-23 01:25:40 +0530

    Time: 0 ms (100%), Space: 46.3 MB (92.34%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -2,26 +2,52 @@ class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
-        boolean flag = true;
-        for(int i=0; i<m-1; i++){
-            int row = i;
-            for(int j=0; j<n-1; j++){
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                
+                int row = i;
                 int col = j;
-                if(matrix[i][j] != matrix[i+1][j+1]){
-                    flag = false;
-                    break;
-                }
                 int val = matrix[i][j];
-                // while(row < m && col < n){
-                //     if(matrix[row][col] != val){
-                //         flag = false;
-                //         break;
-                //     }
-                //     row++;
-                //     col++;
-                // }
+
+                while(row < m && col < n){
+                    if(matrix[row][col] != val){
+                        return false;   // immediately return
+                    }
+                    row++;
+                    col++;
+                }
             }
         }
-        return flag;
+
+        return true;
     }
 }
+
+// class Solution {
+//     public boolean isToeplitzMatrix(int[][] matrix) {
+//         int m = matrix.length;
+//         int n = matrix[0].length;
+//         boolean flag = true;
+//         for(int i=0; i<m-1; i++){
+//             int row = i;
+//             for(int j=0; j<n-1; j++){
+//                 int col = j;
+//                 // if(matrix[i][j] != matrix[i+1][j+1]){
+//                 //     flag = false;
+//                 //     break;
+//                 // }
+//                 int val = matrix[i][j];
+//                 while(row < m && col < n){
+//                     if(matrix[row][col] != val){
+//                         flag = false;
+//                         break;
+//                     }
+//                     row++;
+//                     col++;
+//                 }
+//             }
+//         }
+//         return flag;
+//     }
+// }
