36c04254ec8056d8c7e8dab64567e103d46017dc
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-23 01:28:16 +0530

    Time: 0 ms (100%), Space: 46.5 MB (44.39%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.java
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.java
@@ -12,7 +12,7 @@ class Solution {
 
                 while(row < m && col < n){
                     if(matrix[row][col] != val){
-                        return false;   // immediately return
+                        return false;  
                     }
                     row++;
                     col++;
@@ -30,21 +30,10 @@ class Solution {
 //         int n = matrix[0].length;
 //         boolean flag = true;
 //         for(int i=0; i<m-1; i++){
-//             int row = i;
 //             for(int j=0; j<n-1; j++){
-//                 int col = j;
-//                 // if(matrix[i][j] != matrix[i+1][j+1]){
-//                 //     flag = false;
-//                 //     break;
-//                 // }
-//                 int val = matrix[i][j];
-//                 while(row < m && col < n){
-//                     if(matrix[row][col] != val){
-//                         flag = false;
-//                         break;
-//                     }
-//                     row++;
-//                     col++;
+//                 if(matrix[i][j] != matrix[i+1][j+1]){
+//                     flag = false;
+//                     break;
 //                 }
 //             }
 //         }
