8e13d92f3796cefb34633d91d13adbaef9ab970c
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-02-23 01:42:37 +0530

    Time: 102 ms (28.92%), Space: 86.3 MB (99.38%) - LeetHub

---
+++ 2895-minimum-processing-time/2895-minimum-processing-time.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int minProcessingTime(List<Integer> processorTime, List<Integer> tasks) {
+        Collections.sort(tasks,Collections.reverseOrder());
+        Collections.sort(processorTime);
+        int n = tasks.size();
+        int maxTime = 0;
+        int taskIndex = 0;
+        for(int i=0; i<processorTime.size(); i++){
+            int currentmax = 0;
+            for(int j=0; j<4; j++){
+                currentmax = Math.max(currentmax, processorTime.get(i) + tasks.get(taskIndex));
+                taskIndex += 1;
+            }
+            maxTime = Math.max(maxTime,currentmax);
+        }
+        return maxTime;
+
+
+
+        // int fi = processorTime.get(0);
+        // int la = processorTime.get(1);
+        // for(int i=0; i<n/2; i++){
+        //     if(la + tasks.get(i) > fmax){
+        //         fmax = la + tasks.get(i);
+        //     }
+        // }
+        // for(int i=n/2; i<n; i++){
+        //     if(fi + tasks.get(i) > lmax){
+        //         lmax = fi + tasks.get(i);
+        //     }
+        // }
+        // return Math.max(lmax,fmax);
+    }
+}
