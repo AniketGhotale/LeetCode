3a9343ac8544851a496b07857631a251d966949a
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-20 01:48:40 +0530

    Time: 1 ms (97.9%), Space: 43.9 MB (30.77%) - LeetHub

--- 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
+++ 2138-divide-a-string-into-groups-of-size-k/2138-divide-a-string-into-groups-of-size-k.java
@@ -18,34 +18,7 @@ class Solution {
                 sb.append(fill);
             }
             arr.add(sb.toString());
-        }
-        // if(k%s.length() == 0){
-        //     int n = k/s.length();
-        //     int f = 0;
-        //     int l = k;
-        //     for(int i=0; l<s.length(); i+=k){
-        //         arr.add(s.substring(f,l));
-        //         f = l;
-        //         l+=k;
-        //     }
-        // }else{
-        //     int n = k/s.length();
-        //     int f = 0;
-        //     int l = k;
-        //     for(int i=0; l<s.length(); i+=k){
-        //         arr.add(s.substring(f,l));
-        //         f = l;
-        //         l+=k;
-        //     }
-        //     String str = "";
-        //     for(int i=f;i<s.length(); i++){
-        //         str = str + s.charAt(i);
-        //     }
-        //     for(int i=str.length()-1; i<k; i++){
-        //         str = str + fill;
-        //     }
-        //     arr.add(str);
-        // }   
+        }  
         String res[] = new String[arr.size()];
         for(int i=0; i<arr.size(); i++){
             res[i] = arr.get(i);
