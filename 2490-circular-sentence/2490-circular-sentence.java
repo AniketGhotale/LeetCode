367315761c4e815e2034062edd884b79ae5dc470
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-23 00:02:50 +0530

    Time: 3 ms (11.71%), Space: 43.2 MB (87.78%) - LeetHub

---
+++ 2490-circular-sentence/2490-circular-sentence.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        String arr[] = sentence.split(" ");
+        int n = arr.length;
+        for(int i=1; i<n; i++){
+            if(arr[i-1].charAt(arr[i-1].length()-1) != arr[i].charAt(0)){
+                return false;
+            }
+        }
+        if(arr[0].charAt(0) != arr[n-1].charAt(arr[n-1].length()-1)){
+            return false;
+        }
+
+        return true;
+    }
+}
