48a7aa8a3928ad8b50f1bbe2278380c2ded740fe
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-29 01:38:05 +0530

    Time: 0 ms (100%), Space: 44.7 MB (52.4%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -3,10 +3,10 @@ class Solution {
         int f = 0;
         int s = 0;
         for(int i=0; i<nums.length; i++){
-            if(nums[i] >= f){
+            if(nums[i] > f){
                 s = f;
                 f = nums[i];
-            }else if(nums[i] >= s){
+            }else if(nums[i] > s){
                 s = nums[i];
             }
         }
