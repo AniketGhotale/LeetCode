4ba2930f0cdffdad2688dee96cf1eaae49f4b782
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-29 00:09:10 +0530

    Time: 1 ms (24.58%), Space: 42.8 MB (80.73%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxDepth(String s) {
+        Stack<Character> st = new Stack<>();
+        int max = 0;
+        int count = 0;
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i) == '('){
+                st.push('(');
+                count++;
+            }else if(!s.isEmpty() && s.charAt(i) == ')'){
+                st.pop();
+                count--;
+            }else if(s.charAt(i) == ')'){
+                count--;
+            }
+            max = max >= count ? max : count;
+        }
+        return max;
+    }
+}
