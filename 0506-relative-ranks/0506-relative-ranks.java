e08892b03bb4cb75f087ec7426976d9af8d3bb45
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-30 01:19:20 +0530

    Time: 13 ms (32.91%), Space: 48 MB (15.67%) - LeetHub

--- 0506-relative-ranks/0506-relative-ranks.java
+++ 0506-relative-ranks/0506-relative-ranks.java
@@ -2,17 +2,13 @@ class Solution {
     public String[] findRelativeRanks(int[] score) {
         int n = score.length;
         String[] res = new String[n];
-
-        // Step 1: store value -> index
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
             map.put(score[i], i);
         }
 
-        // Step 2: sort scores
         Arrays.sort(score);
 
-        // Step 3: assign ranks
         int rank = 1;
         for (int i = n - 1; i >= 0; i--) {
             int idx = map.get(score[i]);
