3a3bd84dd167fd126db389aa8e0c14d60382547c
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-31 01:50:21 +0530

    Time: 6 ms (89.47%), Space: 47.2 MB (80.26%) - LeetHub

--- 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.java
@@ -2,8 +2,8 @@ class Solution {
     public double trimMean(int[] arr) {
         Arrays.sort(arr);
         int n = arr.length;
-        //int k = n / 20;
-        int k = (int)Math.ceil(n * 0.05);
+        int k = n / 20;
+        //int k = (int)Math.ceil(n * 0.05);
         long sum = 0;
         for(int i=k; i<n-k; i++){
             sum += arr[i];
