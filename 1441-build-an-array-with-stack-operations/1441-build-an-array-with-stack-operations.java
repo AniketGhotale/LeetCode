1958702ee5a03e347e254698c83015f4484f1bc9
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-03-31 22:00:41 +0530

    Time: 1 ms (14.55%), Space: 44.7 MB (10.32%) - LeetHub

--- 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.java
@@ -1,18 +1,20 @@
 class Solution {
     public List<String> buildArray(int[] target, int n) {
         ArrayList<String> list = new ArrayList<>();
-        Stack<Integer> s = new Stack<>();
+        //Stack<Integer> s = new Stack<>();
+        int count = 0;
         HashSet<Integer> set = new HashSet<>();
         for(int i=0;i<target.length; i++){
             set.add(target[i]);
         }
         for(int i=1; i<=n; i++){
-            if(s.size() == target.length){
+            if(count == target.length){
                 return list;
             }
             if(set.contains(i)){
                 list.add("Push");
-                s.push(i);
+                count++;
+                //s.push(i);
             }else{
                 list.add("Push");
                 list.add("Pop");
