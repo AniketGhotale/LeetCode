f1c844c865ef7a87564ac92987eed48d20a10c43
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-04-02 22:10:22 +0530

    Time: 30 ms (21.21%), Space: 47.2 MB (54%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.java
@@ -2,9 +2,7 @@ class Solution {
     public int minLengthAfterRemovals(String s) {
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++){
-            if(  !st.isEmpty() && (s.charAt(i) == 'a' && st.peek() == 'b' )){
-                st.pop();
-            }else if(!st.isEmpty() && (s.charAt(i) == 'b' && st.peek() == 'a' )){
+            if(  !st.isEmpty() && (s.charAt(i) != st.peek())){
                 st.pop();
             }else{
                 st.push(s.charAt(i));
