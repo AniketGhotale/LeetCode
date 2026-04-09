e420a55d07c5f12f924b34e38c93084f86073290
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-04-10 01:43:43 +0530

    Time: 0 ms (100%), Space: 43.6 MB (64.63%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.java
@@ -1,30 +1,47 @@
 class Solution {
     public int countElements(int[] nums) {
         int n = nums.length;
-        if(n <= 2){
-            return 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] > max){
+                max = nums[i];
+            }
+            if(nums[i] < min){
+                min = nums[i];
+            }
         }
-        Arrays.sort(nums);
-        int f = 1;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] < max && nums[i] > min){
+                count++;
+            }
+        }
+        return count;
+        // if(n <= 2){
+        //     return 0;
+        // }
+        // Arrays.sort(nums);
+        // int f = 1;
         
-        int l = 1;
-        for(int i=0; i<n-1; i++){
-            if(nums[i] == nums[i+1]){
-                f++;
-            }else{
-                break;
-            }
-        }
-        for(int i=n-1; i>0; i--){
-            if(nums[i] == nums[i-1]){
-                l++;
-            }else{
-                break;
-            }
-        }
-        if(f == n){
-            return 0;
-        }
-        return n - f - l;
+        // int l = 1;
+        // for(int i=0; i<n-1; i++){
+        //     if(nums[i] == nums[i+1]){
+        //         f++;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // for(int i=n-1; i>0; i--){
+        //     if(nums[i] == nums[i-1]){
+        //         l++;
+        //     }else{
+        //         break;
+        //     }
+        // }
+        // if(f == n){
+        //     return 0;
+        // }
+        // return n - f - l;
     }
 }
