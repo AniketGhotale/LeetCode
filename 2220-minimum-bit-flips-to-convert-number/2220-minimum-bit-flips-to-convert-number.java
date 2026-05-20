7bcbd1bf5cd2474585a0235444e68836408654ca
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-05-21 01:23:25 +0530

    Time: 5 ms (1.02%), Space: 43.6 MB (5.48%) - LeetHub

---
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minBitFlips(int start, int goal) {
+        String b1 = getbinary(start);
+        String b2 = getbinary(goal);
+        int count = 0;
+        int diff = b1.length() - b2.length();
+        if(diff > 0){
+            b2 = addzero(diff,b2);
+        }else if(diff < 0){
+            b1 = addzero(Math.abs(diff),b1);
+        }
+        
+        for(int i=0; i<b1.length(); i++){
+            if(b1.charAt(i) != b2.charAt(i)){
+                count++;
+            }
+        }
+        
+        return count;
+    }
+
+    String getbinary(int n){
+        String binary = "";
+        while(n > 0){
+            binary = (n % 2) + binary;
+            n/=2;
+        }
+        return binary;
+    }
+
+    String addzero(int n, String s){
+        for(int i=0; i<n;i++){
+            s = '0' + s;
+        }
+        return s;
+    }
+}
