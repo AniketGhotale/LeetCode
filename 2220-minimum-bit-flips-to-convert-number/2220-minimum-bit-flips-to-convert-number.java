87da25c3f163e328fc02998712527f011d764873
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-05-21 01:27:30 +0530

    Time: 4 ms (1.08%), Space: 42.8 MB (5.48%) - LeetHub

--- 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
+++ 2220-minimum-bit-flips-to-convert-number/2220-minimum-bit-flips-to-convert-number.java
@@ -2,21 +2,23 @@ class Solution {
     public int minBitFlips(int start, int goal) {
         String b1 = getbinary(start);
         String b2 = getbinary(goal);
-        int count = 0;
-        int diff = b1.length() - b2.length();
-        if(diff > 0){
-            b2 = addzero(diff,b2);
-        }else if(diff < 0){
-            b1 = addzero(Math.abs(diff),b1);
-        }
+        int x = start ^ goal;
+        return Integer.bitCount(x);
+        // int count = 0;
+        // int diff = b1.length() - b2.length();
+        // if(diff > 0){
+        //     b2 = addzero(diff,b2);
+        // }else if(diff < 0){
+        //     b1 = addzero(Math.abs(diff),b1);
+        // }
         
-        for(int i=0; i<b1.length(); i++){
-            if(b1.charAt(i) != b2.charAt(i)){
-                count++;
-            }
-        }
+        // for(int i=0; i<b1.length(); i++){
+        //     if(b1.charAt(i) != b2.charAt(i)){
+        //         count++;
+        //     }
+        // }
         
-        return count;
+        //return count;
     }
 
     String getbinary(int n){
