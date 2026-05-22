3e548d387f9cdb2d5866d555e72f5bd163da08a1
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-05-23 01:44:49 +0530

    Time: 3 ms (81.9%), Space: 56.3 MB (27.07%) - LeetHub

---
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        int res[][] = score;
+        int n= res.length;
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n-i-1; j++){
+                if(res[j][k] <= res[j+1][k]){
+                    int temp[] = res[j];
+                    res[j] = res[j+1];
+                    res[j+1] = temp;
+                }
+            }
+        }
+        return res;
+    }
+}
