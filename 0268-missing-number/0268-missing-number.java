0f818fa4644a822ea2d3b97bd96482e8ef159983
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-05-27 01:37:55 +0530

    Time: 0 ms (100%), Space: 47.5 MB (31.55%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -8,6 +8,9 @@ class Solution {
         int total = n * (n-1) /2;
         return total - sum;
     
+
+
+    
         // HashMap<Integer,Integer> map = new HashMap<>();
         // for(int i=0; i<nums.length; i++){
         //     map.put(nums[i] , i);
