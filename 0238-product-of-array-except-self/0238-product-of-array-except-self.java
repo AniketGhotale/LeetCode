185986c70c0db1f934ddcbcaa932006162cf095a
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-05-30 01:45:44 +0530

    Time: 2 ms (95.2%), Space: 72.3 MB (15.6%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -2,36 +2,41 @@ class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int[] answer = new int[n];
-        // int sum = 1;
-        // boolean flag = false;
-        // for(int i=0; i<n; i++){
-        //     if(nums[i] != 0){
-        //         sum = sum * nums[i];
-        //     }else{
-        //         flag= true;
-        //     }
+        int sum = 1;
+       int count = 0;
+        for(int i=0; i<n; i++){
+            if(nums[i] != 0){
+                sum = sum * nums[i];
+            }else{
+                count++;
+            }
             
-        // }
-        // for(int i=0; i<n; i++){
-        //     if(nums[i] != 0 && (flag)){
-        //         answer[i] = 0;
-        //     }else if(nums[i] != 0){
-        //         answer[i] = sum / nums[i];
-        //     }else{
-        //         answer[i] = sum;
-        //     }
-            
-        // }
-        answer[0] = 1;
-        for (int i = 1; i < n; i++) {
-            answer[i] = answer[i - 1] * nums[i - 1];
         }
+        for(int i=0; i<n; i++){
+            if(count>1){
+                answer[i] = 0;
+            }else if(count == 1){
+                if(nums[i] != 0){
+                    answer[i] = 0;
+                }else{
+                    answer[i] = sum;
+                }
+            }else{
+                answer[i] = sum / nums[i];
+            }
+            
+            
+        }
+        // answer[0] = 1;
+        // for (int i = 1; i < n; i++) {
+        //     answer[i] = answer[i - 1] * nums[i - 1];
+        // }
 
-        int suffix = 1;
-        for (int i = n - 1; i >= 0; i--) {
-            answer[i] = answer[i] * suffix;
-            suffix = suffix * nums[i];
-        }
+        // int suffix = 1;
+        // for (int i = n - 1; i >= 0; i--) {
+        //     answer[i] = answer[i] * suffix;
+        //     suffix = suffix * nums[i];
+        // }
 
         return answer;
     }
