d15742267f59fe20c05ec3239e1c53156c838944
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-08-06 01:02:23 +0530

    Time: 1 ms (85.03%), Space: 43.2 MB (6.32%) - LeetHub

---
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -0,0 +1,55 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+
+        int f = 0;
+        int s = 0;
+        char lastt = name.charAt(0);
+        while(s < typed.length()){
+            if(name.charAt(f) == typed.charAt(s)){
+                lastt = name.charAt(f);
+                f++;
+                s++;
+                
+            }else if(typed.charAt(s) != lastt){
+                return false;
+                
+            }else{
+                s++;
+            }
+            if(f == name.length()){
+                break;
+            }
+        }
+        char last = name.charAt(name.length()-1);
+
+        while(s < typed.length()){
+            if(typed.charAt(s) != last){
+                return false;
+            }
+            s++;
+        }
+        return f == name.length();
+        // if( (name.length() == typed.length()) && (name.indexOf(typed) == -1)){
+        //     return false;
+        // }
+        // boolean flag = true;
+        // int arr[] = new int[26];
+        // int k=0;
+        // int n=name.length();
+        // for(int i=0; i<typed.length(); i++){
+        //     arr[typed.charAt(i) - 'a']++;
+        //     if(k < n){
+        //         arr[name.charAt(k) - 'a']--;
+        //         k++;
+        //     }
+        // }
+
+        // for(int i=0; i<26; i++){
+        //     if(arr[i] < 0){
+        //         flag = false;
+        //     }
+        // }
+        
+        // return flag;
+    }
+}
