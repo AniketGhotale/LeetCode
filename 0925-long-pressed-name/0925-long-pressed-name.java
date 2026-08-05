4ef15864168dc0ab80f75040ba9fce4567818559
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-08-06 01:07:41 +0530

    Time: 1 ms (85.09%), Space: 42.8 MB (74.74%) - LeetHub

--- 0925-long-pressed-name/0925-long-pressed-name.java
+++ 0925-long-pressed-name/0925-long-pressed-name.java
@@ -3,14 +3,14 @@ class Solution {
 
         int f = 0;
         int s = 0;
-        char lastt = name.charAt(0);
+        char last = name.charAt(0);
         while(f < name.length() && s < typed.length()){
             if(name.charAt(f) == typed.charAt(s)){
-                lastt = name.charAt(f);
+                last = name.charAt(f);
                 f++;
                 s++;
                 
-            }else if(typed.charAt(s) != lastt){
+            }else if(typed.charAt(s) != last){
                 return false;
                 
             }else{
@@ -18,7 +18,7 @@ class Solution {
             }
             
         }
-        char last = name.charAt(name.length()-1);
+        last = name.charAt(name.length()-1);
 
         while(s < typed.length()){
             if(typed.charAt(s) != last){
@@ -27,27 +27,6 @@ class Solution {
             s++;
         }
         return f == name.length();
-        // if( (name.length() == typed.length()) && (name.indexOf(typed) == -1)){
-        //     return false;
-        // }
-        // boolean flag = true;
-        // int arr[] = new int[26];
-        // int k=0;
-        // int n=name.length();
-        // for(int i=0; i<typed.length(); i++){
-        //     arr[typed.charAt(i) - 'a']++;
-        //     if(k < n){
-        //         arr[name.charAt(k) - 'a']--;
-        //         k++;
-        //     }
-        // }
-
-        // for(int i=0; i<26; i++){
-        //     if(arr[i] < 0){
-        //         flag = false;
-        //     }
-        // }
         
-        // return flag;
     }
 }
