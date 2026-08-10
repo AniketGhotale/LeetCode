649b8e5da17ab82b38e5bda68755469e2e21aaae
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-08-11 00:23:26 +0530

    Time: 3 ms (97.77%), Space: 42.3 MB (32.21%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        // if(c < 3){
+        //     return true;
+        // }
+        long num = (long)Math.ceil(Math.sqrt(c));
+        int low = 0;
+        long high = num;
+        while(low <= high){
+            long sum = (low*low) + (high*high);
+            if(sum < c){
+                low++;
+            }else if(sum > c){
+                high--;
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+}
