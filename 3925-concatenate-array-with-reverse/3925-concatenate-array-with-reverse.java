3702e3a2bb184e7c5236fbbc1a74f6a00cf8c989
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-08-16 00:06:59 +0530

    Time: 1 ms (99.21%), Space: 47.3 MB (45.75%) - LeetHub

--- 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
+++ 3925-concatenate-array-with-reverse/3925-concatenate-array-with-reverse.java
@@ -4,19 +4,27 @@ class Solution {
         int res[] = new int[n*2];
         //res = nums.clone();
         int top = 0;
-        int rev = n - 1;
+        int rev = (n*2) - 1;
         // for(int i=0; i<n; i++){
         //     res[i] = res[2*n -1 -i] = nums[i];
         // }
-        for(int i=0; i<n*2; i++){
-            if(top < n){
-                res[i] = nums[top];
-                top++;
-            }else if(top == n){
-                res[i] = nums[rev];
-                rev--;
-            }
+        int i = 0;
+        while(top < rev){
+            res[top] = nums[i];
+            res[rev] = nums[i];
+            i++;
+            top++;
+            rev--;
         }
+        // for(int i=0; i<n*2; i++){
+        //     if(top < n){
+        //         res[i] = nums[top];
+        //         top++;
+        //     }else if(top == n){
+        //         res[i] = nums[rev];
+        //         rev--;
+        //     }
+        // }
         return res;
     }
 }
