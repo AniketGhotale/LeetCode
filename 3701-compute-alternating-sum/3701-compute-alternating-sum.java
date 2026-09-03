68952c415d359e381044a4d9ae7bd1f7460460c7
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-09-04 00:15:34 +0530

    Time: 1 ms (97.13%), Space: 46.2 MB (83.2%) - LeetHub

--- 3701-compute-alternating-sum/3701-compute-alternating-sum.java
+++ 3701-compute-alternating-sum/3701-compute-alternating-sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int alternatingSum(int[] nums) {
         int res = 0;
-
         for(int i=0; i<nums.length; i++){
-            if(i%2 == 0){
+            if(i % 2 == 0){
                 res += nums[i];
             }else{
-                res -= nums[i];
+                res-= nums[i];
             }
         }
-
         return res;
     }
 }
