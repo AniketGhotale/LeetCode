a39f3d275743b4edb5d16ae8a491741b82decb7d
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-09-04 00:28:15 +0530

    Time: 2 ms (95.27%), Space: 46.4 MB (85.33%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.java
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.java
@@ -5,7 +5,7 @@ class Solution {
         int n = words.length;
         for(String word : words){
             int res = getres(word,weights);
-            int idx = 122 - res;
+            int idx = 'z' - res;
             sb.append((char) idx);
         }
         return sb.toString();
@@ -13,7 +13,7 @@ class Solution {
     int getres(String s, int[] arr){
         int res = 0;
         for(char ch : s.toCharArray()){
-            int idx = ch - 97;
+            int idx = ch - 'a';
             res += arr[idx];
         }
         return res % 26;
