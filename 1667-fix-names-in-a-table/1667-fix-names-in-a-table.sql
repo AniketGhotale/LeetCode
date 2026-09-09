99bd3cd6ccdd51b8efa89ef2f069ff02dc2187fe
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-09-10 01:45:02 +0530

    Time: 787 ms (32.5%), Space: 0B (100%) - LeetHub

---
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+-- UPDATE Users SET name  = CONCAT(UPPER(SUBSTRING(name,1,1)), LOWER(SUBSTRING(name,2)));
+SELECT user_id, CONCAT(UPPER(SUBSTRING(name,1,1)), LOWER(SUBSTRING(name,2))) AS name FROM Users ORDER BY user_id;
