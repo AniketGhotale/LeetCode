5ab201615af832fea48d423b623ddc45054f9d1d
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-09-11 02:11:45 +0530

    Time: 618 ms (12.21%), Space: 0B (100%) - LeetHub

---
+++ 0178-rank-scores/0178-rank-scores.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+SELECT * FROM ( SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS `rank` FROM Scores) x;
+
+-- SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS rank FROM Scores;
