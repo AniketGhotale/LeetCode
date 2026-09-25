f6c52d40f90f6aabdc93eeec2aec201c60458691
AniketGhotale <137469864+AniketGhotale@users.noreply.github.com>
2026-09-26 01:33:53 +0530

    Time: 44 ms (98.78%), Space: 146 MB (40.59%) - LeetHub

--- 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.java
+++ 4014-minimum-total-price-after-applying-discounts/4014-minimum-total-price-after-applying-discounts.java
@@ -1,35 +1,46 @@
 class Solution {
     public double minPrice(int[] prices, int[] discounts) {
         Arrays.sort(prices);
-        int s = 0;
-        int h = prices.length-1;
-        while(s < h){
-            int temp = prices[s];
-            prices[s] = prices[h];
-            prices[h] = temp;
-            s++;
-            h--;
-
-        }
         Arrays.sort(discounts);
-        s = 0;
-        h = discounts.length - 1;
-        while(s < h){
-            int temp = discounts[s];
-            discounts[s] = discounts[h];
-            discounts[h] = temp;
-            s++;
+        // int s = 0;
+        // int h = prices.length-1;
+        // while(s < h){
+        //     int temp = prices[s];
+        //     prices[s] = prices[h];
+        //     prices[h] = temp;
+        //     s++;
+        //     h--;
+
+        // }
+        // Arrays.sort(discounts);
+        // s = 0;
+        // h = discounts.length - 1;
+        // while(s < h){
+        //     int temp = discounts[s];
+        //     discounts[s] = discounts[h];
+        //     discounts[h] = temp;
+        //     s++;
+        //     h--;
+        // }
+        // int l = prices.length > discounts.length ? discounts.length : prices.length;
+        // double res = 0;
+        // for(int i=0; i<l; i++){
+        //     res = res + ( prices[i] * (100 - discounts[i]) / 100.0);
+        // }
+
+
+        int l = prices.length > discounts.length ? discounts.length : prices.length;
+        int s = prices.length-1;
+        int h = discounts.length-1;
+        double res = 0;
+        while(s >= 0 && h >= 0){
+            res = res + (prices[s] * (100.0 - discounts[h]) / 100);
+            s--;
             h--;
         }
-        int l = prices.length > discounts.length ? discounts.length : prices.length;
-        double res = 0;
-        for(int i=0; i<l; i++){
-            res = res + ( prices[i] * (100 - discounts[i]) / 100.0);
-        }
-
         
 
-        for(int i=l; i<prices.length; i++){
+        for(int i=s; i>=0; i--){
             res += prices[i];
         }
         return res;
